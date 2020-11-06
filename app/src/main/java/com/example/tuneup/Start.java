aa00b0c502a9bbac0e6c14de7a2d0631606e3db3
Korneliusz Dyszczynski <korneliusz.dyszczynski@gmail.com>
2020-11-06 11:36:28 +0100

    fix

--- app/src/main/java/com/example/tuneup/Start.java
+++ app/src/main/java/com/example/tuneup/Start.java
@@ -14,11 +14,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Start#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Start extends Fragment {
 
 
@@ -35,21 +31,7 @@ public class Start extends Fragment {
         return inflater.inflate(R.layout.fragment_start2, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        final NavController navController = Navigation.findNavController(view);
-
-        Button button = view.findViewById(R.id.next);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_start2_to_home2);
-            }
 
 
-        });
-    }
 
 }
