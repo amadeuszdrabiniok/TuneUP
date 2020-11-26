a33e3784dd6f6c33e88d162c42ae003015c9d04e
Korneliusz Dyszczynski <korneliusz.dyszczynski@gmail.com>
2020-11-26 13:27:03 +0100

    profile

--- app/src/main/java/com/example/tuneup/Dashboard.java
+++ app/src/main/java/com/example/tuneup/Dashboard.java
@@ -1,23 +1,25 @@
 package com.example.tuneup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
+
+
 
     }
 
