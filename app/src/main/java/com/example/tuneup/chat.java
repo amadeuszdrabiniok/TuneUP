dd6872ed6faded730467481839c1f0a11547780b
Amadeusz Drabiniok <amadeusz.drabiniok@gmail.com>
2020-12-03 15:48:19 +0100

    Working chat prototype

--- app/src/main/java/com/example/tuneup/chat.java
+++ app/src/main/java/com/example/tuneup/chat.java
@@ -2,9 +2,8 @@ package com.example.tuneup;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,28 +14,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.SecretKeySpec;
 
 public class chat extends AppCompatActivity {
 
     private DatabaseReference databaseReference;
 
     private EditText messageInput;
-    private ListView messageList;
-
+    private TextView messageOutput;
     private String messageString;
-    private byte encryptionKey[] = {31, 43, 113, -41, 32, -1, 25, 111, 42, 95, -25, 121, 42, 76, -23, 55};
-    private Cipher cipher, decipher;
-    private SecretKeySpec secretKeySpec;
 
 
     @Override
@@ -45,42 +31,18 @@ public class chat extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         messageInput = findViewById(R.id.messageInput);
-        messageList = findViewById(R.id.messageList);
+        messageOutput = findViewById(R.id.messageOutput);
 
         try {
             databaseReference = FirebaseDatabase.getInstance().getReference("Message");
-
-            try {
-                cipher = Cipher.getInstance("AES");
-                decipher = Cipher.getInstance("AES");
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            } catch (NoSuchPaddingException e) {
-                e.printStackTrace();
-            }
-
-            secretKeySpec = new SecretKeySpec(encryptionKey, "AES");
 
             databaseReference.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
 
                     messageString = snapshot.getValue().toString();
-                    messageString = messageString.substring(1, messageString.length()-1);
-
-                    String[] messageStringArray = messageString.split(", ");
-                    String[] messageStringFinal = new String[messageStringArray.length*2];
-
-                    try {
-                        for (int i = 0; i< messageStringArray.length; i++){
-                            String[] stringKeyValue = messageStringArray[i].split("=",2);
-                            messageStringFinal[2 * i] = (String) android.text.format.DateFormat.format("dd-MM-yyyy hh:mm:ss", Long.parseLong(stringKeyValue[0]));
-                            messageStringArray[2 * i + 1] = AESDecryptionMethod(stringKeyValue[1]);
-                        }
-                        messageList.setAdapter(new ArrayAdapter<String>(chat.this, android.R.layout.simple_list_item_1, messageStringFinal));
-                    } catch (NumberFormatException e) {
-                        e.printStackTrace();
-                    }
+                    String[] stringMessageArray = messageString.split("=",2);
+                    messageOutput.setText(stringMessageArray[1]);
 
                 }
 
@@ -96,63 +58,10 @@ public class chat extends AppCompatActivity {
 
     }
 
-    public void sendButton(View view){
+    public void sendButton(View view) {
 
         Date date = new Date();
-        databaseReference.child(Long.toString(date.getTime())).setValue(AESEncryptionMethod(messageInput.getText().toString()));
+        databaseReference.child(Long.toString(date.getTime())).setValue(messageInput.getText().toString());
         messageInput.setText("");
-    }
-
-    private String AESEncryptionMethod(String string){
-
-        byte[] stringByte = string.getBytes();
-        byte[] encryptedByte = new byte[stringByte.length];
-
-        try {
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-            encryptedByte = cipher.doFinal(stringByte);
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        }
-
-        String returnString = null;
-        try {
-            returnString = new String(encryptedByte, "ISO_8859_1");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
-        return returnString;
-    }
-
-    private String AESDecryptionMethod(String string) {
-
-        byte[] encryptedByte = new byte[0];
-        try {
-            encryptedByte = string.getBytes("ISO_8859_1");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        String decryptedString = string;
-
-        byte[] decryption;
-
-        try {
-            decipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-            decryption = decipher.doFinal(encryptedByte);
-            decryptedString = new String(decryption);
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (BadPaddingException e) {
-            e.printStackTrace();
-        } catch (IllegalBlockSizeException e) {
-            e.printStackTrace();
-        }
-
-        return decryptedString;
     }
 }
