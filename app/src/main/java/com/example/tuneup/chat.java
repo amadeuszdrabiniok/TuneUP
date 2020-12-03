bd0dcd27b0729895d05edbd1a257b6956ff7929f
Amadeusz Drabiniok <amadeusz.drabiniok@gmail.com>
2020-12-04 00:11:51 +0100

    Group Chat

--- app/src/main/java/com/example/tuneup/chat.java
+++ app/src/main/java/com/example/tuneup/chat.java
@@ -16,7 +16,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
 public class chat extends AppCompatActivity {
@@ -29,6 +31,7 @@ public class chat extends AppCompatActivity {
     private ListView list;
     private ArrayAdapter<String> adapter;
     private ArrayList<String> arrayList;
+    SimpleDateFormat sdf1 = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
 
 
     @Override
@@ -49,12 +52,43 @@ public class chat extends AppCompatActivity {
         try {
             databaseReference = FirebaseDatabase.getInstance().getReference("Message");
 
+
             databaseReference.addValueEventListener(new ValueEventListener() {
+
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
 
                     messageString = snapshot.getValue().toString();
-                    arrayList.add(messageString);
+                    messageString = messageString.substring(1,messageString.length()-1);
+
+                    String[] stringMessageArray = messageString.split(", ");
+                    Arrays.sort(stringMessageArray);
+                    String[] stringFinal = new String[stringMessageArray.length*2];
+
+                    adapter.clear();
+
+                    try {
+                        for (int i = 0; i < stringMessageArray.length; i++) {
+                            String[] stringKeyValue = stringMessageArray[i].split("=", 2);
+                            stringFinal[2 * i] = sdf1.format(Long.parseLong(stringKeyValue[0]));
+                            stringFinal[2 * i + 1] = stringKeyValue[1];
+                        }
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    int temp = 1, temp2 = 0;
+                    String[] displayString = new String[stringFinal.length/2];
+                    for (int i = 0; i < stringFinal.length; i++){
+                        if (temp%2==0){
+                            displayString[temp2] = stringFinal[i-1]+" "+stringFinal[i];
+                            arrayList.add(displayString[temp2]);
+                            temp2++;
+                        }
+                        temp++;
+                    }
+
                     adapter.notifyDataSetChanged();
                 }
 
