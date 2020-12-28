e8add2a98aacfb506b075fdbb4a505807291942d
Korneliusz Dyszczynski <korneliusz.dyszczynski@gmail.com>
2020-12-28 21:13:08 +0100

    profil

--- app/src/main/java/com/example/tuneup/Dashboard.java
+++ app/src/main/java/com/example/tuneup/Dashboard.java
@@ -7,6 +7,19 @@ import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 public class Dashboard extends AppCompatActivity {
 
 
--- app/src/main/java/com/example/tuneup/profile.java
+++ app/src/main/java/com/example/tuneup/profile.java
@@ -1,32 +1,44 @@
 package com.example.tuneup;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.concurrent.Executor;
 
 
 public class profile extends Fragment {
-
-    String name, email, phone;
+    FirebaseFirestore fStore;
     TextView result;
-
-    EditText nameInput;
-    EditText emailInput;
-    EditText phoneInput;
-
+    TextView fullName,email,phone;
+    StorageReference storageReference;
+    String userId;
+    FirebaseAuth fAuth;
+    FirebaseUser user;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -44,26 +56,33 @@ public class profile extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        nameInput = (EditText) view.findViewById(R.id.nameInput);
-        emailInput = (EditText) view.findViewById(R.id.emailInput);
-        phoneInput = (EditText) view.findViewById(R.id.phoneInput);
-        result = (TextView) view.findViewById(R.id.result);
+        phone = view.findViewById(R.id.phoneInput);
+        fullName = view.findViewById(R.id.nameInput);
+        email = view.findViewById(R.id.emailInput);
 
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        userId = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
 
-
-
-
-        Button button4 = view.findViewById(R.id.save);
-        button4.setOnClickListener(new View.OnClickListener() {
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
             @Override
-            public void onClick(View v) {
-                name = nameInput.getText().toString();
-                email = emailInput.getText().toString();
-                phone = phoneInput.getText().toString();
-                result.setText(name + " " + email+ " " + phone);
-            }
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if(documentSnapshot.exists()){
+                    phone.setText(documentSnapshot.getString("phone"));
+                    fullName.setText(documentSnapshot.getString("fName"));
+                    email.setText(documentSnapshot.getString("email"));
 
+                }
+            }
         });
+
+
+
+
+
 
 
         final NavController navController = Navigation.findNavController(view);
