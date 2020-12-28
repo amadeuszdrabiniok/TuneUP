d6c4a28e78c3b3258bd9ea42fc82fd7f655d948c
Korneliusz Dyszczynski <korneliusz.dyszczynski@gmail.com>
2020-12-28 15:37:10 +0100

    rejestracja

--- app/src/main/java/com/example/tuneup/MainActivity.java
+++ app/src/main/java/com/example/tuneup/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                Intent intent = new Intent(MainActivity.this,Register.class);
                 startActivity(intent);
                 finish();
 
