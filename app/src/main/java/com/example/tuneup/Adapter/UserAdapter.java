96b3d00d8785f5fdd4f2fdc924e63be4966043fd
Korneliusz Dyszczynski <korneliusz.dyszczynski@gmail.com>
2021-01-12 13:43:12 +0100

    profile uzytkowanikow

--- app/src/main/java/com/example/tuneup/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/tuneup/Adapter/UserAdapter.java
@@ -7,13 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.tuneup.Model.Userm;
+import com.example.tuneup.OthersProfile;
 import com.example.tuneup.R;
 import com.google.firebase.firestore.auth.User;
 
@@ -38,7 +42,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Userm user = mUsers.get(position);
+        final Userm user = mUsers.get(position);
         holder.username.setText(user.getUsername());
         if (user.getImageURL().equals("default")){
             holder.profileImage.setImageResource(R.drawable.bez_nazwy_2);
@@ -46,6 +50,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
             Glide.with(mContext).load(user.getImageURL()).into(holder.profileImage);
         }
 
+        holder.itemView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, OthersProfile.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
