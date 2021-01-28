bcaaa67ce4ea226953810edcf3f48e31ee1fa422
Amadeusz Drabiniok <amadeusz.drabiniok@gmail.com>
2021-01-28 17:52:56 +0100

    klucz

--- app/src/main/java/com/example/tuneup/Notifications/APIService.java
+++ app/src/main/java/com/example/tuneup/Notifications/APIService.java
@@ -9,7 +9,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAv8TNNwc:AAAAv8TNNwc:APA91bGnduM0zG4RUZdgGvSoVF6g7fkKgLgKnaNo1Y4bVxmQs-dulRc-J3ucYjxnqy8kk2kh8Wlq4k3UaZ2guz36tBjFvVEM7q9Su39Hq9ybKokwclfTNJ6Sw6dluN_ymVo6ORqEPMEp"
+                    "Authorization:key=AAAAv8TNNwc:APA91bGnduM0zG4RUZdgGvSoVF6g7fkKgLgKnaNo1Y4bVxmQs-dulRc-J3ucYjxnqy8kk2kh8Wlq4k3UaZ2guz36tBjFvVEM7q9Su39Hq9ybKokwclfTNJ6Sw6dluN_ymVo6ORqEPMEp"
             }
     )
 
